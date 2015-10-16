dec883fa3654a69ef44ffa193333c1863fa01415
zagv <zlate55@yahoo.com>
2015-10-16 18:34:27 +1100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -52,7 +52,6 @@ public class Main {
                     		cdv.get(0), cdv.get(1), cdv.get(2));
                     
                     if (winnings > 0) {
-                        //player.receiveWinnings(winnings);
 	                    System.out.printf("%s won %d, balance now %d\n\n",
 	                    		player.getName(), winnings, player.getBalance());
 	                	winCount++; 
