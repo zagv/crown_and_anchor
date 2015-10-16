0ba61c0aa5309451d755cdce9e33884fb0b5404a
quachvu <nomatterhow21@yahoo.com>
2015-10-16 19:16:35 +1100

    Fix bug #6: The SPADE has never come up in the GAME

--- src/DiceValue.java
+++ src/DiceValue.java
@@ -22,7 +22,7 @@ public enum DiceValue {
 	}
 	
 	public static DiceValue getRandom() {
-		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
+		int random = RANDOM.nextInt(VALUE_REPR_MAP.size());
 		return values()[random];
 	}
 	
